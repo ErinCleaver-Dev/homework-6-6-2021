d4449801a6d1086082497ccef591f64556d3a6aa
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-06 11:29:34 -0400

    update

--- index.js
+++ index.js
@@ -39,17 +39,20 @@ class LinkList {
  append(value) {
  
     const node = new Node(value, this.head);
+    let count = 0;
     if(!this.head) {
         this.head = node;
-        this.size += 1
+        this.size ++;
     } else {
+       
+        
         var current = this.head;
         while(current.next) {
-            current = current.next
+            current = current.next;
         }
-       current.next = node;
-       this.tail = node;
-       this.size ++;
+        this.tail = current
+        this.tail.next = this.node;
+        this.size ++;
     }
 
     return this;
@@ -59,15 +62,29 @@ class LinkList {
  find(findValue) {
 
     var current = this.head
-    while (current != null) {
-        if(current.data == findValue) {
-            return `Found ${findValue}`;
+    while (current.next) {
+        console.log(current)
+        if(current.element == findValue) {
+            return `The value of ${findValue} was found.`;
         }
         current = current.next;
     }
-    return false;
+    return `The value of ${findValue} was not found.`;
  }
- remove() {
+
+ // removing the last value
+ removeLast() {
+    if(!this.tail) {
+        return "No values in list"
+    } else {
+        let current = this.tail;
+
+        
+
+    } 
+
+
+
 
  }
  reverse(){
@@ -80,8 +97,10 @@ linkedList = new LinkList();
 
 linkedList.prepend(45);
 linkedList.prepend(12);
-linkedList.append(15);
+linkedList.prepend(15);
 linkedList.append(45);
-console.log(linkedList);
-console.log(linkedList.find(12));
-console.log(linkedList.find(78));
+linkedList.append(12);
+
+console.log(linkedList.find(78))
+console.log(linkedList.find(12))
+console.log(JSON.stringify(linkedList));
